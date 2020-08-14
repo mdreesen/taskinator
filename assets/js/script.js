5d109aef804499fc80f790a5978ba39b5e7fb3c5
Michael Dreesen <michaeldreesen90@gmail.com>
2020-08-14 11:49:42 -0600

    added new code to the script to look at the form and not button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,15 @@
 // These are referencing the id's in the html
-buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-/* buttonEl.addEventListener("click", function() {
-    //Create a new task item
-    var listItemEl = document.createElement("li");
-    //Style the new task item
-    listItemEl.className = "task-item";
-    // Add the text
-    listItemEl.textContent = "This is a new task.";
-    // Append this element to the task list
-    tasksToDoEl.appendChild(listItemEl);
-});
-*/
+var createTaskHandler = function(event) {
+    event.preventDefault();
 
-var createTaskHandler = function() {
-    //Create a new task item
     var listItemEl = document.createElement("li");
-    //Style the new task item
     listItemEl.className = "task-item";
-    // Add the text
     listItemEl.textContent = "This is a new task.";
-    // Append this element to the task list
     tasksToDoEl.appendChild(listItemEl);
 }
 
-buttonEl.addEventListener("click", createTaskHandler);
+
+formEl.addEventListener("submit", createTaskHandler);
